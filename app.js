b5c2a5b4ccc48cc72b89ea96c8c03e4f2cf3a568
Omerntosi <omer_naveed@yahoo.com>
2018-03-25 20:15:54 -0500

    Added comments

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+   //Navbar buttons
    $(".tab").click(function() {
       if($(this).hasClass("disabled") != true) {
          $(this).siblings().removeClass("active");
@@ -7,24 +8,28 @@ $(document).ready(function() {
       };
    });
    
+   //Btn size adjust
    if($(window).width() <= 600) {
       $(".waves-effect").addClass("btn");
    } else {
       $(".waves-effect").addClass("btn-large");
    }
 
+   //Lives
    let lives = 10;
    function liveCount(lives) {
       let livesStr = lives.toString();
       $(".flow-text:first").text("Lives: " + livesStr);
    }
    
-
+   //Word generator
+   //Run through JSON & choose random word
    let actual = "";
    let temp = "";
    let easyWords = "easyWords.json";
+
    function generateEasy(easyWords) {
-      $.getJSON(easyWords, function(data) {
+      $.getJSON(easyWords, function(data) { 
          let wordVal = Math.floor(Math.random() * data.length);
          actual = data[wordVal].word.toUpperCase();
          for (let i = 0; i < actual.length; i++) {
@@ -37,16 +42,14 @@ $(document).ready(function() {
          $(".flow-text:nth-child(2)").text(temp);
       });
    };
-   
-   generateEasy(easyWords);
-   liveCount(10);
 
+   //Words guessing
    $(".waves-effect").click(function() {
-      let letter = $(this).text()
-      if (actual.indexOf(letter) == -1) {
+      let letter = $(this).text() //Letter guessed by user
+      if (actual.indexOf(letter) == -1) { //If letter DNE in word
          lives--;
          liveCount(lives);
-      } else {
+      } else { //Letter is in word
          for (let i = 0; i < actual.length; i++) {
             if (actual.substring(i,i+1) == letter) {
                let posOfLetter = i;
@@ -55,19 +58,23 @@ $(document).ready(function() {
          }
          $(".flow-text:nth-child(2)").text(temp);
       }
-      $(this).addClass("disabled");
-      if(lives == 0) {
+      $(this).addClass("disabled"); //Disable btn after click
+      if(lives == 0) { //If no more lives
          $(".waves-effect").addClass("disabled");
          $(".flow-text:first").text("Game Over");
          $(".flow-text:last").text(actual);
-      } else if (temp == actual) {
+      } else if (temp == actual) { //If solved
          $(".waves-effect").addClass("disabled");
          $(".flow-text:first").text("You Win!");
       }
    });
 
+   generateEasy(easyWords);
+   liveCount(10);
+
    //TODO
    //Implement score
    //Retry btn
+   //Keyboard support
 });
 
