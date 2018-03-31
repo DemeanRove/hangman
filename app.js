f999ea37d063623cd5ad0f790b94d41773a8f8f0
Omerntosi <omer_naveed@yahoo.com>
2018-03-31 00:50:38 -0500

    Fixed js formatting + added minor support for medium mode

--- app.js
+++ app.js
@@ -1,158 +1,71 @@
 $(document).ready(function() {
+    
+    //Scale in animations
+    $(".waves-effect").addClass("scale-in")
+    $(".game").addClass("scale-in")
+    
+    //Navbar buttons
+    $(".tab").click(function() {
+        if ($(this).hasClass("disabled") != true) {
+            $(this).siblings().removeClass("active")
+            $(this).addClass("active")
+        }
+    })
+    
+    //Btn size adjust
+    if ($(window).width() <= 600) {
+        $(".waves-effect").addClass("btn")
+    } 
+    else {
+        $(".waves-effect").addClass("btn-large")
+    }
+    
+    //Play Again Btn
+    // 
+    // TODO : Change to work for different levels of difficulty
+    //     
+    $(".wrapper").click(function() {
+        $(".waves-effect").removeClass("disabled")
+        reset()
+    })
+    
+    //On-Screen Btn Pushed
+    $(".waves-effect").click(function() { 
+        let letter = $(this).text() //Letter guessed by user
+        letterCheck(letter, level)
+        $(this).addClass("disabled") //Disable btn after click
+    })
+  
+    //Key pressed
+    $(window).keypress(function(e) {
+        if (lives != 0) {
+            var c = String.fromCharCode(e.which).toUpperCase() 
+            if (jQuery.inArray(c, guessed) == -1) { //If key not already guessed
+                let letter = c //Letter guessed by user
+                letterCheck(letter, level)
+                $(".waves-effect").filter(function() {
+                    return $(this).text() == c;
+                }).addClass("disabled")
+            }
+        }
+    })
 
-   let guessed = []
+   //Game Mode Select
+   $(".tab-easy").click(function() {
+       level = ".easy"
+       reset()
+   })
 
-   let score = 0;
+   $(".tab-medium").click(function() {
+       level = ".medium"
+       resetMed()
+   })
 
-   function updateScore() {
-       $(".score").text("Score: " + score);
-   }
+   //Final Calls
+   reset()
 
-   $(".waves-effect").addClass("scale-in");
-   $(".game").addClass("scale-in");
-
-   //Navbar buttons
-   $(".tab").click(function() {
-       if ($(this).hasClass("disabled") != true) {
-           $(this).siblings().removeClass("active");
-           $(this).addClass("active");
-       };
-   });
-
-   //Btn size adjust
-   if ($(window).width() <= 600) {
-       $(".waves-effect").addClass("btn");
-   } else {
-       $(".waves-effect").addClass("btn-large");
-   }
-
-   //Lives
-   let lives = 10;
-
-   function liveCount(lives) {
-       let livesStr = lives.toString();
-       $(".flow-text:first").text("Lives: " + livesStr);
-   }
-
-   //Word generator
-   //Run through JSON & choose random word
-   let actual = "";
-   let temp = "";
-   let easyWords = "easyWords.json";
-
-   function generateEasy(easyWords) {
-       temp = "";
-       actual = "";
-       $.getJSON(easyWords, function(data) {
-           let wordVal = Math.floor(Math.random() * data.length);
-           actual = data[wordVal].word.toUpperCase();
-           for (let i = 0; i < actual.length; i++) {
-               if (actual.substring(i, i + 1) == " ") {
-                   temp += " ";
-               } else {
-                   temp += "_"
-               }
-           };
-           $(".flow-text:nth-child(3)").text(temp);
-       });
-   };
-
-   //Words guessing
-   $(".waves-effect").click(function() {
-       let letter = $(this).text() //Letter guessed by user
-       guessed.push(letter);
-       if (actual.indexOf(letter) == -1) { //If letter DNE in word
-           lives--;
-           liveCount(lives);
-       } else { //Letter is in word
-           for (let i = 0; i < actual.length; i++) {
-               if (actual.substring(i, i + 1) == letter) {
-                   let posOfLetter = i;
-                   temp = temp.substring(0, posOfLetter) + letter + temp.substring(posOfLetter + 1);
-                   score += 100;
-                   updateScore();
-               }
-           }
-           $(".flow-text:nth-child(3)").text(temp);
-       }
-       $(this).addClass("disabled"); //Disable btn after click
-       if (lives == 0) { //If no more lives
-           gameOver(false);
-       } else if (temp == actual) { //If solved
-           gameOver(true);
-       }
-   });
-
-   $(window).keypress(function(e) {
-       if (lives != 0) {
-           var c = String.fromCharCode(e.which).toUpperCase() // or e.keyCode
-           if (jQuery.inArray(c, guessed) == -1) {
-               //do stuff with "key" here...
-               let letter = c; //Letter guessed by user
-               if (actual.indexOf(letter) == -1) { //If letter DNE in word
-                   lives--;
-                   liveCount(lives);
-               } else { //Letter is in word
-                   for (let i = 0; i < actual.length; i++) {
-                       if (actual.substring(i, i + 1) == letter) {
-                           let posOfLetter = i;
-                           temp = temp.substring(0, posOfLetter) + letter + temp.substring(posOfLetter + 1);
-                           score += 100;
-                           updateScore();
-                       }
-                   }
-                   $(".flow-text:nth-child(3)").text(temp);
-               }
-               $(".waves-effect").filter(function() {
-                   return $(this).text() == c;
-               }).addClass("disabled");
-
-               if (lives == 0) { //If no more lives
-                   gameOver(false);
-               } else if (temp == actual) { //If solved
-                   gameOver(true);
-               }
-               guessed.push(c);
-           }
-       }
-   });
-
-   function gameOver(flag) {
-       $(".waves-effect").addClass("disabled");
-       $(".wrapper").addClass("scale-in");
-       if (flag) {
-           $(".flow-text:nth-child(2)").text("You Win!");
-           score += lives * 150;
-           updateScore();
-       } else {
-           $(".flow-text:nth-child(2)").text("Game Over");
-           let over = "";
-           for (let i = 0; i < actual.length; i++) {
-               if ($(".flow-text:nth-child(3)").text().substring(i, i + 1) == "_") {
-                   over += "<span style='color: red'>" + actual.substring(i, i + 1) + "</span>";
-               } else {
-                   over += $(".flow-text:nth-child(3)").text().substring(i, i + 1);
-               }
-           }
-           $(".flow-text:nth-child(3)").html(over);
-       }
-   }
-
-   $(".wrapper").click(function() {
-       generateEasy(easyWords);
-       lives = 10;
-       liveCount(10);
-       $(".waves-effect").removeClass("disabled");
-       $(".wrapper").removeClass("scale-in");
-       score = 0;
-   });
-
-   generateEasy(easyWords);
-   updateScore();
-   liveCount(10);
-   $('.tooltipped').tooltip();
-});
-
-//To-Do
-//Find a way to make btn more mobile friendly
-//Implement medium & hard modes
+   //Materialize CSS Commands
+   $('.tooltipped').tooltip()
+   $('.tabs').tabs()
+        
+})
---
+++ mechanics.js
@@ -0,0 +1,137 @@
+//Variables 
+
+let score = 0
+let guessed = [] //Guessed letters
+let lives = 10
+let easyWords = "easyWords.json"
+let mediumWords = "mediumWords.json"
+let hardWords = "easyWords.json"
+let temp = ""
+let actual = ""
+let level = ".easy"
+
+//Guessing Mechanic
+
+function letterCheck(guessedLetter) {
+   guessed.push(guessedLetter)
+   if (actual.indexOf(guessedLetter) == -1) { //If letter DNE in word
+      lives--
+      liveCount(lives)
+   } 
+   else { //Letter is in word
+      for (let i = 0; i < actual.length; i++) {
+         if (actual.substring(i, i + 1) == guessedLetter) {
+            let posOfLetter = i
+            temp = temp.substring(0, posOfLetter) + guessedLetter + temp.substring(posOfLetter + 1)
+            score += 100
+            updateScore()
+         }
+      }
+      $(level).text(temp) //Append word so far to div
+   }
+
+   if (lives == 0) { //If no more lives
+      gameOver(false)
+   } else if (temp == actual) { //If solved
+      gameOver(true)
+   }
+}
+
+//Score Management
+
+function updateScore() {
+   $(".score").text("Score: " + score)
+}
+
+//Lives Management
+function liveCount(lives) {
+   let livesStr = lives.toString()
+   $(".flow-text:first").text("Lives: " + livesStr)
+}
+
+//Game Over Mechanic
+function gameOver(winner) {
+   $(".waves-effect").addClass("disabled")
+   $(".wrapper").addClass("scale-in")
+   if (winner) {
+      $(".flow-text:nth-child(2)").text("You Win!")
+      score += lives * 150
+      updateScore()
+   } 
+   else {
+      $(".flow-text:nth-child(2)").text("Game Over")
+      let over = ""
+      for (let i = 0; i < actual.length; i++) {
+         if ($(level).text().substring(i, i + 1) == "_") {
+            over += "<span style='color: red'>" + actual.substring(i, i + 1) + "</span>"
+         } 
+         else {
+            over += $(level).text().substring(i, i + 1);
+         }
+      }
+      $(level).html(over)
+   }
+}
+
+//Word Generators
+//Run through JSON & choose random word
+function generateEasy(easyWords) {
+   temp = ""
+   $.getJSON(easyWords, function(data) {
+      let wordVal = Math.floor(Math.random() * data.length)
+      actual = data[wordVal].word.toUpperCase()
+      for (let i = 0; i < actual.length; i++) {
+         if (actual.substring(i, i + 1) == " ") {
+            temp += " "
+         } 
+         else {
+            temp += "_"
+         }
+      }
+      $(".easy").text(temp)
+   })
+}
+
+function generateMedium(mediumWords) {
+   temp = ""
+   $.getJSON(mediumWords, function(data) {
+      let wordVal = Math.floor(Math.random() * data.length)
+      actual = data[wordVal].word.toUpperCase()
+      for (let i = 0; i < actual.length; i++) {
+         if (actual.substring(i, i + 1) == " ") {
+            temp += " "
+         } 
+         else {
+            temp += "_"
+         }
+      }
+      $(".medium").text(temp)
+   })
+}
+
+// Reset function
+function reset() {
+   temp = ""
+   actual = ""
+   guessed = []
+   lives = 10
+   score = 0
+   generateEasy(easyWords)
+   updateScore()
+   liveCount(10)
+   $(".waves-effect").removeClass("disabled")
+   $(".wrapper").removeClass("scale-in")
+}
+
+function resetMed() {
+   temp = ""
+   actual = ""
+   guessed = []
+   lives = 10
+   score = 0
+   generateMedium(mediumWords)
+   updateScore()
+   liveCount(10)
+   $(".waves-effect").removeClass("disabled")
+   $(".wrapper").removeClass("scale-in")
+}
